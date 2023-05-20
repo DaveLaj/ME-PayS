f3f7df36fcd896548ecfd3aecad828e0dae571d2
Shamsahamnida <charmainejune.balanay@g.msuiit.edu.ph>
2023-05-20 08:43:59 +0800

    Placeholders

--- ME-PAYS/me_pays_app/forms.py
+++ ME-PAYS/me_pays_app/forms.py
@@ -6,13 +6,13 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MaxLengthValidator
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}),required=True, help_text='Required.')
-    first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'maxlength': '30'}),required=True, help_text='Required.')
-    last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),required=True, help_text='Required.')
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    contact_number = forms.CharField(widget=forms.NumberInput(attrs={'class':'form-control', 'id': 'contact_number', 'oninput':"this.value = this.value.slice(0, 10);"}),required=True, validators=[MaxLengthValidator(10, message='Please enter a 10-digit contact number.')], help_text='Required.')
-    school_id= forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control'}),required=True, validators=[MaxLengthValidator(8, message='Please correct the format of your ID.')], help_text='Required.')
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control form-control-sm', 'placeholder':'Enter My.IIT email'}),required=True, help_text='Required.')
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control form-control-sm', 'placeholder':'Enter first name', 'maxlength': '30'}),required=True, help_text='Required.')
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control form-control-sm', 'placeholder':'Enter last name'}),required=True, help_text='Required.')
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control form-control-sm', 'placeholder':'Enter password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control form-control-sm', 'placeholder':'Confirm password'}))
+    contact_number = forms.CharField(widget=forms.NumberInput(attrs={'class':'form-control form-control-sm', 'placeholder':'9xxxxxxxxx', 'id': 'contact_number', 'oninput':"this.value = this.value.slice(0, 10);"}),required=True, validators=[MaxLengthValidator(10, message='Please enter a 10-digit contact number.')], help_text='Required.')
+    school_id= forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control form-control-sm', 'placeholder':'20190001'}),required=True, validators=[MaxLengthValidator(8, message='Please correct the format of your ID.')], help_text='Required.')
 
     class Meta:
         model = CustomUser
@@ -23,13 +23,13 @@ class RegisterForm(UserCreationForm):
 
 
 class POS_CreationForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'maxlength':'100'}),required=True, help_text='Required.')
-    store_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'maxlength': '30'}),required=True, help_text='Required.')
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    contact_number = forms.CharField(widget=forms.NumberInput(attrs={'class':'form-control limit-length', 'id': 'contact_number', 'oninput':"this.value = this.value.slice(0, 10);"}),required=True, validators=[MaxLengthValidator(10, message='Please enter a 10-digit contact number.')], help_text='Required.')
-    location = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'maxlength': '100'}),required=True, help_text='Required.')
-    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), required=False)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control form-control-sm', 'maxlength':'100'}),required=True, help_text='Required.')
+    store_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control form-control-sm', 'maxlength': '30'}),required=True, help_text='Required.')
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control form-control-sm'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control form-control-sm'}))
+    contact_number = forms.CharField(widget=forms.NumberInput(attrs={'class':'form-control form-control-sm limit-length', 'id': 'contact_number', 'oninput':"this.value = this.value.slice(0, 10);"}),required=True, validators=[MaxLengthValidator(10, message='Please enter a 10-digit contact number.')], help_text='Required.')
+    location = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control form-control-sm', 'maxlength': '100'}),required=True, help_text='Required.')
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control form-control-sm', 'rows':'5'}), required=False)
 
     class Meta:
         model = CustomUser
@@ -72,8 +72,8 @@ class POS_CreationForm(UserCreationForm):
 
             
 class POS_ChangePassword(UserChangeForm):
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'New Password', 'name':'password1'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Confirm New Password', 'name':'password2'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control form-control-sm', 'placeholder':'New Password', 'name':'password1'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control form-control-sm', 'placeholder':'Confirm New Password', 'name':'password2'}))
     class Meta(UserChangeForm.Meta):
         model = CustomUser
         fields = ('password1', 'password2')
