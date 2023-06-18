2f28d8e88b1f67df0b3e35716eaa16d40c34b35c
Dave Miller Lapojapo <lapojapod@gmail.com>
2023-06-19 00:02:32 +0800

    For ERD

--- ME-PAYS/me_pays_app/models/order.py
+++ ME-PAYS/me_pays_app/models/order.py
@@ -1,11 +1,11 @@
 from django.db import models
 import json
 import random
-
+from me_pays_app.models.order import EndUser
 class Order(models.Model):
     items = models.TextField()
     reference_number = models.CharField(max_length=20, unique=True, null=True)
-    school_id = models.IntegerField(null=True)
+    enduser = models.OneToOneField(EndUser, on_delete=models.CASCADE)
     paid = models.BooleanField(default=False)
     def set_items(self, items):
         self.items = json.dumps(items)
